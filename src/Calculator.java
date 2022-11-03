2a8be4583be0e75d22d820cd5d2bb595b5a50293
Alp Senay <alps2789@nycstudents.net>
2022-11-03 12:31:13 -0400

    working...

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double n1, n2;
+
+    public Calculator(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public double multiply() {
+        if (((int) (n1) == n1) && ((int) (n2) == n2)) {
+            return (int) (n1*n2);
+        } else {
+            return n1*n2;
+        }
+    }
+
+    public double divide() {
+        if (((int) (n1) == n1) && ((int) (n2) == n2)) {
+            if (n1%n2 == 0) {
+                return (int) (n1/n2);
+            } else {
+                return n1/n2;
+            }
+        } else {
+            return n1/n2;
+        }
+    }
+
+    public double add() {
+        if (((int) (n1) == n1) && ((int) (n2) == n2)) {
+            return (int) (n1+n2);
+        } else {
+            return n1+n2;
+        }
+    }
+
+    public double subtract() {
+        if (((int) (n1) == n1) && ((int) (n2) == n2)) {
+            return (int) (n1-n2);
+        } else {
+            return n1-n2;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an expression with *, /, +, -, %, or ^: ");
+        String request = scan.nextLine();
+
+        double num1 = Double.parseDouble(request.substring(0,request.indexOf(" ")));
+        double num2 = Double.parseDouble(request.substring(request.indexOf(" ")+3));
+
+
+    }
 }
